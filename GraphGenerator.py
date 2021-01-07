d6b9440ff0fc6de12750f080a6f71e5e65ff3e3e
teo_ad <thodoris.adamantidis@gmail.com>
2021-01-07 12:18:55 +0200

    Add GraphClass Simple graph implementation into GraphGenerator

---
+++ GraphGenerator.py
@@ -0,0 +1,54 @@
+from GraphClass import Graph
+
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+    return edges
+
+def find_isolated_nodes(graph):
+    """ returns a list of isolated nodes. """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
+
+if __name__ == "__main__":
+    g = {"a": ["c"],
+             "b": ["c", "e"],
+             "c": ["a", "b", "d", "e"],
+             "d": ["c"],
+             "e": ["c", "b"],
+             "f": []
+             }
+
+    graph = Graph(g)
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print("Add vertex:")
+    graph.add_vertex("z")
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Add an edge:")
+    graph.add_edge({"a", "z"})
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print('Adding an edge {"x","y"} with new vertices:')
+    graph.add_edge({"x", "y"})
+    print("Vertices of graph:")
+    print(graph.vertices())
+    print("Edges of graph:")
+    print(graph.edges())
