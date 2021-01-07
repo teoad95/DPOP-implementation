f3830cc2f493efa70a7d08ba137f8e9a613689f1
teo_ad <thodoris.adamantidis@gmail.com>
2021-01-08 00:12:38 +0200

    Implemented main class functionality. Added nunit.

--- PseudoTreeClass.py
+++ PseudoTreeClass.py
@@ -1,5 +1,6 @@
 import random
 
+
 class PseudoTree(object):
 
     def __init__(self, graph):
@@ -7,10 +8,53 @@ class PseudoTree(object):
         self.PseudoNodes = []
 
     def PseudoTreeCreation(self):
+        # elect leader randomly
+        root_node = self.elect_root_node()
+        # create token
+        token = Token(root_node)
+        # construct the tree
+        self.PseudoTreeNodesCreation(root_node, token)
+
+    def elect_root_node(self):
+        root_name = random.choice(self.Graph.vertices())
+        root_node = self.CreateNodeAndAddItOnTree(root_name)
+        return root_node
+
+    def PseudoTreeNodesCreation(self, parent_node, token):
+        for neighbourName in parent_node.Neighbours:
+
+            node_is_parent = next((c for c in parent_node.get_Parent() if c.Name == neighbourName), None)
+            if node_is_parent is not None:
+                continue
+
+            # may we not really need this check
+            node_is_pseudoparent = next((c for c in parent_node.get_PseudoParent() if c.Name == neighbourName), None)
+            if node_is_pseudoparent is not None:
+                continue
+
+            node_is_pseudochild = next((c for c in parent_node.get_PseudoChild() if c.Name == neighbourName), None)
+            if node_is_pseudochild is not None:
+                continue
+
+            node_already_exists_in_the_tree = next((c for c in self.PseudoNodes if c.Name == neighbourName), None)
+            if node_already_exists_in_the_tree is not None:
+                node_already_exists_in_the_tree.set_PseudoChild(parent_node)
+                parent_node.set_PseudoParent(node_already_exists_in_the_tree)
+            else:
+                child_node = self.CreateNodeAndAddItOnTree(neighbourName)
+                parent_node.set_Child(child_node)
+                child_node.set_Parent(parent_node)
+                # Adding child node name to token in order to loop through child's children
+                token.HavePassedFrom.append(neighbourName)
+                self.PseudoTreeNodesCreation(child_node, token)
+                # Removing child node name from token as we finished with child's children and we go one step back
+                token.HavePassedFrom.remove(neighbourName)
+
+    def CreateNodeAndAddItOnTree(self, name):
         nodesAndNeighbours = self.Graph.getNodesAndNeighbours()
-        # elect leader randonmly
-        root = random.choice(self.Graph.vertices())
-        self._AppendNodeToPseudoTree(PseudoTreeNode(root, nodesAndNeighbours[root], True))
+        rootNode = PseudoTreeNode(name, nodesAndNeighbours[name])
+        self._AppendNodeToPseudoTree(rootNode)
+        return rootNode
 
     def _AppendNodeToPseudoTree(self, ptn):
         if not ptn in self.PseudoNodes:
@@ -19,27 +63,45 @@ class PseudoTree(object):
 
 class PseudoTreeNode(object):
 
-    def __init__(self, name, nodesAndNeighbours, isRoot = False):
+    def __init__(self, name, neighbours):
         self.Name = name
         self._P = []
         self._PP = []
         self._C = []
         self._PC = []
-        self.IsRoot = isRoot
-        self.NodesAndNeighbours = { i : False for i in nodesAndNeighbours }
+        self.Neighbours = neighbours
 
     def set_Parent(self, x):
         if not x in self._P:
             self._P.append(x)
 
+    def get_Parent(self):
+        return self._P
+
     def set_PseudoParent(self, x):
         if not x in self._PP:
             self._PP.append(x)
+
+    def get_PseudoParent(self):
+        return self._PP
 
     def set_Child(self, x):
         if not x in self._C:
             self._C.append(x)
 
+    def get_Child(self):
+        return self._C
+
     def set_PseudoChild(self, x):
         if not x in self._PC:
             self._PC.append(x)
+
+    def get_PseudoChild(self):
+        return self._PC
+
+
+class Token(object):
+
+    def __init__(self, ParentNodes):
+        self.HavePassedFrom = []
+        self.HavePassedFrom.append(ParentNodes)
--- Tests/PseudoTreeTestsClass.py
+++ Tests/PseudoTreeTestsClass.py
@@ -1,6 +1,6 @@
 import unittest
 from GraphClass import Graph
-from PseudoTreeClass import PseudoTree
+from PseudoTreeClass import PseudoTree, Token
 
 class PseudoTreeTests(unittest.TestCase):
 
@@ -18,7 +18,27 @@ class PseudoTreeTests(unittest.TestCase):
 
         #Action
         self.assertFalse(tree.PseudoNodes)
-        tree.PseudoTreeCreation()
+        tree.elect_root_node()
 
         #Assert
         self.assertTrue(tree.PseudoNodes)
+
+    def test_TreeConstruction(self):
+        # Assume
+        g = {"X0": ["X1", "X4"],
+                "X1": ["X0", "X4"],
+                "X4": ["X0", "X1", "X9", "X10"],
+                "X9": [],
+                "X10": []
+              }
+
+        graph = Graph(g)
+        tree = PseudoTree(graph)
+        # Action
+        # We will set ase root node X0
+        root_node = tree.CreateNodeAndAddItOnTree("X0")
+        token = Token("X0")
+        tree.PseudoTreeNodesCreation(root_node, token)
+        # Assert
+        self.assertCountEqual(tree.PseudoNodes, 5)
+        # TODO Add validations for each Pseudonode P, PP, C, PC
