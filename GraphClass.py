443e986ce87df102c83fd4ffbb8ad003351408f2
Christodoulos Bochalis <bojito90@gmail.com>
2021-01-09 00:57:12 +0200

    Implements the problem graph creation from input file.

--- GraphClass.py
+++ GraphClass.py
@@ -31,6 +31,25 @@ class Graph(object):
     def getNodesAndNeighbours(self):
         return self.__graph_dict
 
+    def add_edge(self, edge):
+        """ assumes that edge is of type set, tuple or list;
+            between two vertices can be multiple edges!
+        """
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def add_vertex(self, vertex):
+        """ If the vertex "vertex" is not in
+            self.__graph_dict, a key "vertex" with an empty
+            list as a value is added to the dictionary.
+            Otherwise nothing has to be done.
+        """
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
 
     def __str__(self):
         res = "vertices: "
--- msp_solver.py
+++ msp_solver.py
@@ -1,66 +1,5 @@
 import random
-import graph
-
-class Graph(object):
-
-    def __init__(self, graph_dict=None):
-        """ initializes a graph object
-            If no dictionary or None is given,
-            an empty dictionary will be used
-        """
-        if graph_dict == None:
-            graph_dict = {}
-        self.__graph_dict = graph_dict
-
-    def vertices(self):
-        """ returns the vertices of a graph """
-        return list(self.__graph_dict.keys())
-
-    def edges(self):
-        """ returns the edges of a graph """
-        return self.__generate_edges()
-
-    def add_vertex(self, vertex):
-        """ If the vertex "vertex" is not in
-            self.__graph_dict, a key "vertex" with an empty
-            list as a value is added to the dictionary.
-            Otherwise nothing has to be done.
-        """
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = []
-
-    def add_edge(self, edge):
-        """ assumes that edge is of type set, tuple or list;
-            between two vertices can be multiple edges!
-        """
-        edge = set(edge)
-        (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
-        else:
-            self.__graph_dict[vertex1] = [vertex2]
-
-    def __generate_edges(self):
-        """ A static method generating the edges of the
-            graph "graph". Edges are represented as sets
-            with one (a loop back to the vertex) or two
-            vertices
-        """
-        edges = []
-        for vertex in self.__graph_dict:
-            for neighbour in self.__graph_dict[vertex]:
-                if {neighbour, vertex} not in edges:
-                    edges.append({vertex, neighbour})
-        return edges
-
-    def __str__(self):
-        res = "vertices: "
-        for k in self.__graph_dict:
-            res += str(k) + " "
-        res += "\nedges: "
-        for edge in self.__generate_edges():
-            res += str(edge) + " "
-        return res
+from GraphClass import Graph
 
 
 class Agent:
@@ -89,6 +28,8 @@ class MspSolver(object):
         self.agents = []
         self.meetings = []
         self.variables = {}
+        self.problem_graph = Graph({})
+
 
     def load_problem(self, filename):
 
@@ -173,9 +114,36 @@ class MspSolver(object):
             print("Number of Meetings: " + str(self.numOfMeetings))
             print("Number of Variables: " + str(self.numOfVariables))
 
+    def create_graph(self):
+
+        for var in self.variables:
+            self.problem_graph.add_vertex(var)
+
+        for agent in self.agents:
+            constraints = []
+            for meeting in agent.meetings:
+                constraints.append(agent.name + '_' + meeting.mid)
+            M = len(constraints)
+            for c in range(M):
+                k = constraints.pop()
+                for j in constraints:
+                    self.problem_graph.add_edge({k, j})
+
+        for meeting in self.meetings:
+            constraints = []
+            for p in meeting.participants:
+                constraints.append(p + '_' + meeting.mid)
+
+            M = len(constraints)
+            for c in range(M):
+                k = constraints.pop()
+
+                for j in constraints:
+                    self.problem_graph.add_edge({k, j})
+
     def export_graph(self):
 
-        with open(".\\extra\\MSP_Problem_Graph" + str(self.numOfAgents) + ".txt", "w") as f:
+         with open(".\\extra\\MSP_Problem_Graph_" + str(self.numOfAgents) + ".txt", "w") as f:
             f.write("graph G { " + '\n')
 
             for i in self.variables:
@@ -210,27 +178,31 @@ class MspSolver(object):
             f.write("}")
 
 
-if __name__ == "__main__":
-    g = {"a": ["d"],
-         "b": ["c"],
-         "c": ["b", "c", "d", "e"],
-         "d": ["a", "c"],
-         "e": ["c"],
-         "f": []
-         }
+         with open(".\\extra\\MSP_Problem_Graph_nocolor_" + str(self.numOfAgents) + ".txt", "w") as f:
+            f.write("graph G { " + '\n')
 
-    graph = Graph(g)
+            for vertex in self.problem_graph.vertices():
+                f.write(vertex + '\n')
+
+            f.write('\n')
+
+            _problem_graph_dict = self.problem_graph.getNodesAndNeighbours()
+
+            for vertex in _problem_graph_dict:
+                for item in _problem_graph_dict[vertex]:
+                    f.write(vertex + '--' + item+ '\n')
+
+            f.write("}")
+
+
+if __name__ == "__main__":
+
     MspSolver = MspSolver()
-    MspSolver.load_problem('.\\extra\\MSP_Problem_5 .txt')
+    MspSolver.load_problem('.\\extra\\MSP_Problem_5.txt')
+    MspSolver.create_graph()
     MspSolver.export_graph()
 
-    print("Add an edge:")
-    graph.add_edge({"a", "z"})
 
-    print("Vertices of graph:")
-    print(graph.vertices())
 
-    print("Edges of graph:")
-    print(graph.edges())
 
 
