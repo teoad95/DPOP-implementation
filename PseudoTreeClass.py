Fixes constraint calculation

PseudoTreeClass.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from anytree import NodeMixin, PostOrderIter
 
 class PseudoTree(object):
@@ -57,7 +58,7 @@ class PseudoTree(object):
     def CreateNodeAndAddItOnTree(self, name, parent_node):
         nodesAndNeighbours = self.Graph.getNodesAndNeighbours()
         rootNode = PseudoTreeNode(name, nodesAndNeighbours[name],
-                                  values=self.variable_x_unary_constraint[name],
+                                  var=self.variable_x_unary_constraint[name],
                                   parent=parent_node)
         self._AppendNodeToPseudoTree(rootNode)
         return rootNode
@@ -109,7 +110,7 @@ class PseudoTree(object):
 
 class PseudoTreeNode(NodeMixin):
 
-    def __init__(self, name,  neighbours, values,  parent=None, children=None):
+    def __init__(self, name,  neighbours, var,  parent=None, children=None):
         super(PseudoTreeNode, self).__init__()
         self.name = name
         self._P = []
@@ -117,7 +118,7 @@ class PseudoTreeNode(NodeMixin):
         self._C = []
         self._PC = []
         self._SEP = []
-        self.values = values
+        self.var = var
         self.Neighbours = neighbours
         self.parent = parent
 
@@ -162,14 +163,9 @@ class PseudoTreeNode(NodeMixin):
 
     def compute_binary_constraint(self, v2):
 
-        binary_constraint = [[0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0],
-                             [0,0,0,0,0,0,0,0]]
+
+
+        binary_constraint = np.zeros(shape=(len(self.var.domain), len(self.var.domain)))
 
         v1_agent = self.name.split("_")[0]
         v1_meeting = self.name.split("_")[1]
@@ -179,19 +175,19 @@ class PseudoTreeNode(NodeMixin):
 
         if v1_agent == v2_agent:
             if v1_meeting != v2_meeting:
-                for i in range(len(self.values.utils)):
-                    for j in range(len(v2.values.utils)):
+                for i in range(len(self.var.utils)):
+                    for j in range(len(v2.var.utils)):
                         if i == j:
                             binary_constraint[i][j] = 0
                         else:
-                            binary_constraint[i][j] = int(self.values.utils[i]) + int(v2.values.utils[i])
+                            binary_constraint[i][j] = int(self.var.utils[i]) + int(v2.var.utils[i])
 
         elif v1_meeting == v2_meeting:
             if v1_agent != v2_agent:
-                for i in range(len(self.values.utils)):
-                    for j in range(len(v2.values.utils)):
+                for i in range(len(self.var.utils)):
+                    for j in range(len(v2.var.utils)):
                         if i == j:
-                            binary_constraint[i][j] = int(self.values.utils[i]) + int(v2.values.utils[i])
+                            binary_constraint[i][j] = int(self.var.utils[i]) + int(v2.var.utils[i])
                         else:
                             binary_constraint[i][j] = 0
 